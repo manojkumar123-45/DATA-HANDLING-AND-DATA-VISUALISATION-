d126d0d125381bb95a36c1590362b3091feb0966
manojkumar123-45 <114462006+manojkumar123-45@users.noreply.github.com>
2024-02-01 12:37:04 +0530

    Create 10.box plot using notch.R

---
+++ 10.box plot using notch.R
@@ -0,0 +1,12 @@
+mtcars$cyl_level <- factor(mtcars$cyl, levels = c(4, 6, 8), labels = c("High", "Medium", "Low"))
+
+# Boxplot with custom colors and custom names
+boxplot(mpg ~ cyl_level, data = mtcars,
+        xlab = "Quantity of Cylinders",
+        ylab = "Miles Per Gallon",
+        main = "Boxplot Example",
+        notch = TRUE,
+        varwidth = TRUE,
+        col = c("green", "yellow", "red"),
+        names = c("High", "Medium", "Low")
+)
